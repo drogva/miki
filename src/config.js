629845e22203f2fae8237a28d4209bbd601a770e
drogva <156391770+drogva@users.noreply.github.com>
2024-03-17 22:02:29 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+const { createMikroOrmOptions } = require('./mikro-orm-config');
+
 const configService = {
     DB_NAME: 'kpop',
     DB_USERNAME: 'root',
@@ -6,8 +8,10 @@ const configService = {
     DB_PORT: '3306'
 };
 
+const mikroOrmOptions = createMikroOrmOptions(configService);
+
 module.exports = {
-    ...configService,
+    ...mikroOrmOptions,
     mode: 'development', // 또는 'production'
     // 다른 webpack 구성 옵션들...
 };
