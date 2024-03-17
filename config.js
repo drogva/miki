f201004a26032b285fe989018fe4ea387e7eed48
drogva <156391770+drogva@users.noreply.github.com>
2024-03-17 18:19:57 +0900

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const configService = {
-    DB_NAME: 'database-1',
-    DB_USERNAME: 'root',
-    DB_PASSWORD: 'bizkit87',
-    DB_HOST: 'database-1.cg5x8ymzexwa.ap-northeast-2.rds.amazonaws.com',
-    DB_PORT: '3306'
-};
-
-module.exports = configService;
